f8452770554e36ce7890866807f42725e08eec9d
Xx-IKERO-90-xX <ikerjrh2003@gmail.com>
2024-12-12 22:46:51 +0100

    Creamos un div en la plantilla main para mi profesión

--- scripts/academy/gmedio.js
+++ scripts/academy/gmedio.js
@@ -1,32 +1,31 @@
-let content = document.getElementById('grado_medio');
+let gmContent = document.getElementById('grado_medio');
 
-function clear() {
-    content.innerHTML = "";
+function clearGM() {
+    gmContent.innerHTML = "";
 }
 
-function invokeFirstYear() {
-    clear();
-    
+function invokeFirstYearGM() {
+    clearGM();
+
     let fistYearTemplate = document.getElementById("gm1_template").content.cloneNode(true);
     let fistYearContent = fistYearTemplate.getElementById("gm1_content");
-    content.append(fistYearContent);
+
+    gmContent.append(fistYearContent);
 }
 
-
-function invokeSecondYear() {
-    clear();
-
+function invokeSecondYearGM() {
+    clearGM();
+    
     let secondYearTemplate = document.getElementById("gm2_template").content.cloneNode(true);
     let secondYearContent = secondYearTemplate.getElementById("gm2_content");
-
-    content.append(secondYearContent);
+    
+    gmContent.append(secondYearContent);
 }
-
 
 let btn_gm1 = document.getElementById("btn_gm1");
 let btn_gm2 = document.getElementById("btn_gm2");
 
-btn_gm1.addEventListener("click", invokeFirstYear);
-btn_gm2.addEventListener("click", invokeSecondYear);
+btn_gm1.addEventListener("click", invokeFirstYearGM);
+btn_gm2.addEventListener("click", invokeSecondYearGM);
 
-invokeFirstYear();
+invokeFirstYearGM();
--- scripts/academy/gsuperior.js
+++ scripts/academy/gsuperior.js
@@ -1,31 +1,31 @@
-let content = document.getElementById("grado_superior");
+let gsContent = document.getElementById("grado_superior");
 
-function clear() {
-    content.innerHTML = "";
+function clearGS() {
+    gsContent.innerHTML = "";
 }
 
-function invokeFistYear() {
-    clear();
+function invokeFirstYearGS() {
+    clearGS();
 
     let firstYearTemplate = document.getElementById("gs1_template").content.cloneNode(true);
     let fistYearContent = firstYearTemplate.getElementById("gs1_content");
 
-    content.append(fistYearContent);
+    gsContent.append(fistYearContent);
 }
 
-function invokeSecondYear() {
-    clear();
+function invokeSecondYearGS() {
+    clearGS();
 
     let secondYearTemplate = document.getElementById("gs2_template").content.cloneNode(true);
     let secondYearContent = secondYearTemplate.getElementById("gs2_content");
 
-    content.append(secondYearContent);
+    gsContent.append(secondYearContent);
 }
 
 let btn_gs1 = document.getElementById("btn_gs1");
 let btn_gs2 = document.getElementById("btn_gs2");
 
-btn_gs1.addEventListener("click", invokeFistYear);
-btn_gs2.addEventListener("click", invokeSecondYear);
+btn_gs1.addEventListener("click", invokeFirstYearGS);
+btn_gs2.addEventListener("click", invokeSecondYearGS);
 
-invokeFistYear();
+invokeFirstYearGS();
