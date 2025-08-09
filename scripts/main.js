dbb98588e7dbb94000b3a0beacf42156fa06b955
Xx-IKERO-90-xX <ikerjrh2003@gmail.com>
2025-08-09 21:23:42 +0200

    Creamos de nuevo la parte inicial del portafolio para que quede lo más epico posible

--- scripts/main.js
+++ scripts/main.js
@@ -7,36 +7,10 @@ function clear() {
 function invokeMainTemplate() {
     clear();
 
-    let mainTemplate = document.getElementById('main_template').content.cloneNode(true);
-    let content = mainTemplate.getElementById('mainContent');
+    let mainTemplate = document.getElementById('main-template').content.cloneNode(true);
+    let content = mainTemplate.getElementById('main-content');
 
     contentDiv.append(content);
 }
-
-function invokePanaGamingTemplate() {
-    clear();
-
-    let panaGamingTemplate = document.getElementById('panagaming_template').content.cloneNode(true);
-    let content = panaGamingTemplate.getElementById('panaGamingContent');
-
-    contentDiv.append(content);
-}
-
-function invokeAcademyTemplate() {
-    clear();
-
-    let academyTemplate = document.getElementById('academy_template').content.cloneNode(true);
-    let content = academyTemplate.getElementById('academyContent');
-
-    contentDiv.append(content);
-}
-
-let btnMain = document.getElementById("btn_main");
-let btnProyect = document.getElementById("btn_proyect");
-let btnAcademy = document.getElementById("btn_academy");
-
-btnMain.addEventListener("click", invokeMainTemplate);
-btnProyect.addEventListener("click", invokePanaGamingTemplate);
-btnAcademy.addEventListener("click", invokeAcademyTemplate);
 
 invokeMainTemplate();
