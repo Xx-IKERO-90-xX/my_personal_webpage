4d805b453b4f1dfe7098d0daf6ca1066eca0d721
Xx-IKERO-90-xX <ikerjrh2003@gmail.com>
2025-08-10 14:55:40 +0200

    Creamos la sección donde detallo mi expediente académico

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,20 @@
 let contentDiv = document.getElementById('content');
 
+let btnMain = document.getElementById('btn-main');
+let btnEducation = document.getElementById('btn-education');
+let btnProyectos = document.getElementById('btn-proyectos');
+let btnContacts = document.getElementById('btn-contacts');
+
 function clear() {
     contentDiv.innerHTML = "";
+}
+
+function invokeEducationTemplate() {
+    clear();
+    let educationTemplate = document.getElementById('education-template').content.cloneNode(true);
+    let content = educationTemplate.getElementById('education-content');
+    
+    contentDiv.append(content);
 }
 
 function invokeMainTemplate() {
@@ -12,5 +25,8 @@ function invokeMainTemplate() {
 
     contentDiv.append(content);
 }
+
+btnMain.addEventListener('click', invokeMainTemplate);
+btnEducation.addEventListener('click', invokeEducationTemplate);
 
 invokeMainTemplate();
